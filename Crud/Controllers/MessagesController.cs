921430ab5b4a8f53b937e47778f2ee5bcdb56cca
Sergey <sergey.fokin@arcadia.spb.ru>
2019-11-01 17:48:17 +0300

    complete CRUD

--- Crud/Controllers/MessagesController.cs
+++ Crud/Controllers/MessagesController.cs
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Crud.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
     public class MessagesController : Controller
     {
         private readonly CrudExampleContext _context;
@@ -19,63 +17,42 @@ namespace Crud.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        public ActionResult ListMessages()
         {
-            var result = _context.Messages.Where(m => m.IsDeleted.HasValue && !m.IsDeleted.Value).ToList();
-            return Ok( result);
+            var result = _context.Messages.ToList();
+            return View( result);
         }
 
-        [HttpGet("{id}", Name = "Get")]
-        public IActionResult Get(int id)
+        public ActionResult Details(int id)
         {
-            var result = _context.Messages.Where(m => m.Id == id);
-            return Ok(result);
+            var result = _context.Messages.Where(m => m.Id == id).FirstOrDefault();
+            return View(result);
+        }
+        public ActionResult Create()
+        {
+            return View();
         }
 
         [HttpPost]
-        public IActionResult Post([FromForm] string value)
+        public ActionResult Create(IFormCollection data)
         {
-            var message = new Messages
+            try
             {
-                Date = DateTime.Now,
-                Message = value
-            };
+                var message = new Messages
+                {
+                    Date = DateTime.Now,
+                    Message = data["Message"]
+                };
 
-            _context.Messages.Add(message);
-            _context.SaveChanges();
+                _context.Messages.Add(message);
+                _context.SaveChanges();
 
-            return Ok(message);
-
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromForm] string value)
-        {
-            if(!_context.Messages.Any(m=>m.Id == id))
-            {
-                return NotFound();
+                return RedirectToAction(nameof(ListMessages));
             }
-            var message = _context.Messages.Where(m => m.Id == id).Single();
-            message.Message = value;
-            _context.Update(message);
-            _context.SaveChanges();
-            return Ok();
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            if (!_context.Messages.Any(m => m.Id == id))
+            catch
             {
-                return NotFound();
+                return View();
             }
-
-            var message = _context.Messages.Where(m => m.Id == id).Single();
-            message.IsDeleted = true;
-            _context.Update(message);
-            _context.SaveChanges();
-            return Ok();
         }
     }
 }
